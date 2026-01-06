5919a680c73110a9627963753919596a83af14e2
Anton Zeltser <anton.zeltser@gmail.com>
2026-01-06 15:58:26 +0100

    first commit

---
+++ src/main/java/com/github/scoring/GithubRepositoryScoringApplication.java
@@ -0,0 +1,14 @@
+package com.github.scoring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubRepositoryScoringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GithubRepositoryScoringApplication.class, args);
+        System.out.println("Hello world");
+    }
+
+}
